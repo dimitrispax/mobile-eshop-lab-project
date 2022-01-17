cca7b74cc72d65f741be38e1fed2e1911c8ab3eb
harriskremm <harriskremm@outlook.com>
2022-01-17 18:27:51 +0200

    Updated the specs on the mobile page

--- js/phones_req.js
+++ js/phones_req.js
@@ -13,7 +13,7 @@ function reqListener () {
       data.forEach(element => {       
         
         let div = document.createElement('div');
-        div.classList.add('col-md-4', 'd-flex', 'align-items-stretch');
+        div.classList.add('col-lg-4','mb-3', 'd-flex', 'align-items-stretch');
         row.appendChild(div);
 
         //   <div class="card p-card bg-white p-2 rounded px-3 mt-5">
@@ -26,11 +26,15 @@ function reqListener () {
         items.classList.add('align-items-center', 'credits');
         card.appendChild(items);
 
+        let d_flex = document.createElement('div');
+        d_flex.classList.add('d-flex', 'flex-column');
+        card.appendChild(d_flex);
+
         //  <img class="card-img-top"
         let img = document.createElement('img');
-        img.classList.add('card-img-top');
-        img.src = data[0].photoURL;
-        items.appendChild(img);
+        img.classList.add('image');
+        img.src = element.photoURL;
+        d_flex.appendChild(img);
 
         // <span class="text-black-50 ml-2">
         let span = document.createElement('span');
@@ -40,16 +44,75 @@ function reqListener () {
         // <h5 class="mt-2 d-flex flex-column">
         let h5 = document.createElement('h5');
         h5.classList.add('mt-2', 'd-flex', 'flex-column');
-        h5.textContent = data[0].model;
+        h5.textContent = element.model;
         card.appendChild(h5);
-        
-        // <span class="d-block mb-3">
-        let spanB = document.createElement('span');
-        spanB.classList.add('d-block', 'mb-3');
-        spanB.textContent = "CPU: " + data[0].cpu;
-        card.appendChild(spanB);
 
        
+        let pCpu = document.createElement('p');
+        pCpu.textContent = "CPU: ";
+        card.appendChild(pCpu);
+
+        let spanCPU = document.createElement('span');
+        spanCPU.textContent = element.cpu;
+        spanCPU.classList.add('info-left');
+        pCpu.appendChild(spanCPU);
+
+
+        let pScreenSize = document.createElement('p');
+        pScreenSize.textContent = "Screen Size: ";
+        card.appendChild(pScreenSize);
+
+        let spanScreen = document.createElement('span');
+        spanScreen.textContent = element.screenSize;
+        spanScreen.classList.add('info-left');
+        pScreenSize.appendChild(spanScreen);
+
+        let pCamera = document.createElement('p');
+        pCamera.textContent = "Camera: ";
+        card.appendChild(pCamera);
+
+        let spanCamera = document.createElement('span');
+        spanCamera.textContent = element.camera;
+        spanCamera.classList.add('info-left');
+        pCamera.appendChild(spanCamera);
+
+        let pBattery = document.createElement('p');
+        pBattery.textContent = "Battery: " ;
+        card.appendChild(pBattery);
+
+        let spanBattery = document.createElement('span');
+        spanBattery.textContent = element.battery;
+        spanBattery.classList.add('info-left');
+        pBattery.appendChild(spanBattery);
+
+        let pRam = document.createElement('p');
+        pRam.textContent = "Ram: ";
+        card.appendChild(pRam);
+
+        let spanRam = document.createElement('span');
+        spanRam.textContent = element.ram;
+        spanRam.classList.add('info-left');
+        pRam.appendChild(spanRam);
+
+        let pSar = document.createElement('p');
+        pSar.textContent = "SAR: ";
+        card.appendChild(pSar);
+
+        let spanSar = document.createElement('span');
+        spanSar.textContent = element.sar;
+        spanSar.classList.add('info-left');
+        pSar.appendChild(spanSar);
+
+        let price = document.createElement('p');
+        price.textContent = "Price: ";
+        card.appendChild(price);
+
+        let spanPrice = document.createElement('span');
+        spanPrice.textContent = element.price;
+        spanPrice.classList.add('info-left');
+        price.appendChild(spanPrice);
+
+
 
         // button
         let btn = document.createElement('button');
--- phones.php
+++ phones.php
@@ -17,7 +17,7 @@ session_start();
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <script src="js/main.js"></script>
+    <!-- <script src="js/main.js"></script> -->
     <script src="js/phones_req.js"></script>
     <script src="js/pagin.js"></script>
     <link rel="stylesheet" href="css/phonesStyle.css">
@@ -67,8 +67,7 @@ session_start();
         
             <div class="container mt-5 mb-5 ">
                 <div class="card-content" style="display: none">
-                    <div class="row" id="row">
-                    </div>
+                    <div class="row mt-1" id="row"></div>
                 </div>
                 <div class="pagination mt-4">
                     <li class="page-item previous-page disable"><a class="page-link" href="#">Prev</a></li>
